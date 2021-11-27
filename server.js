7f77fe701d1d40e59bd56a44d1ceb6fc00ae239f
Alexander Leino <Alexander.Leino@yahoo.com>
2021-11-27 02:14:55 -0500

    decided to make a horror api

--- server.js
+++ server.js
@@ -1,39 +1,49 @@
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
-
-
+const { contains } = require('cheerio/lib/static')
 
 const PORT = 3001
 const app = express()
 
 app.use(express.static('public'))
 
-const playerNames = []
 
-const url = 'https://www.packers.com/team/stats/'
 
-axios(url)
-    .then(response => {
+const movies = []
+
+
+
+    axios.get('https://www.esquire.com/entertainment/movies/g3383/best-horror-movies-of-all-time/')
+    .then((response) => {
         const html = response.data
+       
         const $ = cheerio.load(html)
-        $('.nfl-o-roster__player-name',html).each(function(){
-            let name = $(this).text()
-            playerNames.push(name)
-            
-            let playerNamesFiltered = [...new Set(playerNames)]
-            playerNamesFiltered.sort()
-        $()
-            
+        
+        $('.listicle-slide-hed-text', html).each(function(){
+            const title = $(this).text()
+        
+                movies.push({
+                    title: title,
+                })
+                
+                
+               
+                
+                
+
+               
+                
+            })
             
         })
-        
-    })
-    .catch(e => {
-        console.log(e)
-    })
     
+    app.get('/', (req, res) => {
+        console.log('Get Route was hit')
+        res.json(movies)
+    })
+
+
     app.listen(PORT, () => {
         console.log(`Listening on PORT ${PORT}, have fun :) !`)
     })
-   
